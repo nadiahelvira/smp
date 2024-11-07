584a110a7e39854c387d68fbe5bb107444a2b35b
nadiahelvira <nadiahelvira56@gmail.com>
2024-11-07 16:57:52 +0700

    XX

--- resources/views/otransaksi_hut/edit.blade.php
+++ resources/views/otransaksi_hut/edit.blade.php
@@ -17,7 +17,7 @@
         <div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0">Edit Pembayaran Hutang {{$header->NO_BUKTI}}</h1>	
+					<h1 class="m-0">Pembayaran Hutang {{$header->NO_BUKTI}}</h1>	
 				</div>
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
--- resources/views/otransaksi_piu/edit.blade.php
+++ resources/views/otransaksi_piu/edit.blade.php
@@ -17,7 +17,7 @@
         <div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0">Edit Pembayaran Piutang {{$header->NO_BUKTI}}</h1>	
+					<h1 class="m-0"> Pembayaran Piutang {{$header->NO_BUKTI}}</h1>	
 				</div>
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
